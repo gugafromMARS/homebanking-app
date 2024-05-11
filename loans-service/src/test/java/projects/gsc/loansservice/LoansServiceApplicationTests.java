832b77c6f7427e801067bfc397257494f0948dd8
Gonçalo <goncaloscravo@gmail.com>
2024-05-11 17:59:46 +0100

    Add: basic structure

---
+++ loans-service/src/test/java/projects/gsc/loansservice/LoansServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package projects.gsc.loansservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
